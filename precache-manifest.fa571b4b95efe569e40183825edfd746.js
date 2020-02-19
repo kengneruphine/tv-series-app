f8322b0e8a53e06c1739b58a800e178d7d5b177c
kengne ruphine <ruphinekengne@gmail.com>
2020-02-19 18:24:02 +0100

    Updates

---
+++ precache-manifest.fa571b4b95efe569e40183825edfd746.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fc6d493ff9d3905c2e86b5fcb86645c",
+    "url": "/tv-series-app/index.html"
+  },
+  {
+    "revision": "a1b4af1366c6bbf6c345",
+    "url": "/tv-series-app/static/css/main.46c43322.chunk.css"
+  },
+  {
+    "revision": "9149d96e45f1e79324e2",
+    "url": "/tv-series-app/static/js/2.99f4a023.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/tv-series-app/static/js/2.99f4a023.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1b4af1366c6bbf6c345",
+    "url": "/tv-series-app/static/js/main.0abbf694.chunk.js"
+  },
+  {
+    "revision": "a9cd5e5abb89b5764756",
+    "url": "/tv-series-app/static/js/runtime-main.25276e26.js"
+  },
+  {
+    "revision": "eb055ebea0f39a150b330feffbd20d42",
+    "url": "/tv-series-app/static/media/loading.eb055ebe.gif"
+  }
+]);
